5ccdfe242953d71df8dff961b06ebf941c9a3eb2
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-11-16 07:56:55 -0600

    start command API

---
+++ lib/Project/Manager/Command/TravisCI.pm
@@ -0,0 +1,13 @@
+package Project::Manager::Command::TravisCI;
+
+use strict;
+use warnings;
+
+# TODO get subcommands from Travis-CI CLI client
+# <https://github.com/travis-ci/travis.rb>
+# and other command-line tools
+# <http://docs.travis-ci.com/user/apps/#commandline>
+my @sub = qw/enable encrypt/;
+
+
+1;
